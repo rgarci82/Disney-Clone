c25a2cec3045feefac87206471e01cc3103f333a
rgarci82 <ruben.garcia30102@gmail.com>
2023-08-23 13:31:11 -0500

    added the landing page signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
