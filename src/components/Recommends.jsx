328dd562c1ce9241dc543a06b07ac48bca57739f
rgarci82 <ruben.garcia30102@gmail.com>
2023-08-27 02:40:39 -0500

    set up firebase hosting

--- src/components/Recommends.jsx
+++ src/components/Recommends.jsx
@@ -34,10 +34,6 @@ const Content = styled.div`
   gap: 25px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
 
-  @media (max-width: 1024px) {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-  }
-
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
