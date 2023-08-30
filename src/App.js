40e74a73e371743f01c0372b01ebd79fe52fed0d
rgarci82 <ruben.garcia30102@gmail.com>
2023-08-29 22:18:42 -0500

    can only access home page if signed in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
+import { selectUserName } from "../src/features/user/userSlice";
+import { useSelector } from "react-redux";
 
 function App() {
+  const userName = useSelector(selectUserName);
+
   return (
     <div className="App">
       <Router>
         <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/home" element={<Home />} />
+          <Route
+            exact
+            path="/home"
+            element={!userName ? <Navigate to="/" /> : <Home />}
+          />
           <Route path="/detail/:id" element={<Detail />} />
         </Routes>
       </Router>
